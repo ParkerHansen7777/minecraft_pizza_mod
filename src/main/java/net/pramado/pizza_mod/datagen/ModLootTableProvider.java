baed381f5863b875a69d932fdef762ecb74030d9
ParkerHansen7777 <parkerhansen7777@gmail.com>
2023-11-28 12:07:03 -0800

    Added ability for mined items to drop something, loot table generation

--- src/main/java/net/pramado/pizza_mod/datagen/ModLootTableProvider.java
+++ src/main/java/net/pramado/pizza_mod/datagen/ModLootTableProvider.java
@@ -1,4 +1,17 @@
 package net.pramado.pizza_mod.datagen;
 
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.pramado.pizza_mod.datagen.loot.ModBlockLootTables;
+
+import java.util.List;
+import java.util.Set;
+
 public class ModLootTableProvider {
+    public static LootTableProvider create(PackOutput output){
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
+        ));
+    }
 }
--- src/main/java/net/pramado/pizza_mod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/pramado/pizza_mod/datagen/loot/ModBlockLootTables.java
@@ -1,23 +1,32 @@
 package net.pramado.pizza_mod.datagen.loot;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 import net.pramado.pizza_mod.PizzaMod;
+import net.pramado.pizza_mod.block.ModBlocks;
 
-public class ModBlockLootTables extends BlockStateProvider {
-    public ModBlockLootTables(PackOutput output, ExistingFileHelper exFileHelper) {
-        super(output, PizzaMod.MOD_ID, exFileHelper);
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+
+    public ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
     @Override
-    protected void registerStatesAndModels() {
-
+    protected void generate() {
+        this.dropSelf(ModBlocks.SALT_ORE.get());
     }
 
-    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
-        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
     }
 }
