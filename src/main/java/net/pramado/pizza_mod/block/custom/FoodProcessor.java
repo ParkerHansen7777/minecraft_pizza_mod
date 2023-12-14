f872a50d74dc7f2d265bb59c5b2cf37b4065254e
ParkerHansen7777 <parkerhansen7777@gmail.com>
2023-12-14 13:31:09 -0800

    Added new food_processor direction based texture

--- src/main/java/net/pramado/pizza_mod/block/custom/FoodProcessor.java
+++ src/main/java/net/pramado/pizza_mod/block/custom/FoodProcessor.java
@@ -1,17 +1,20 @@
 package net.pramado.pizza_mod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import net.pramado.pizza_mod.block.entity.FoodProcessorBlockEntity;
@@ -19,14 +22,21 @@ import net.pramado.pizza_mod.block.entity.ModBlockEntities;
 import org.jetbrains.annotations.Nullable;
 
  public class FoodProcessor extends BaseEntityBlock {
-   public FoodProcessor(Properties pProperties) {
+
+     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+     public FoodProcessor(Properties pProperties) {
         super(pProperties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
     }
+
+     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+         return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+     }
 
      @Override
      public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
@@ -70,4 +80,10 @@ import org.jetbrains.annotations.Nullable;
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new FoodProcessorBlockEntity(pPos, pState);
     }
+
+     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+         pBuilder.add(FACING);
+     }
+
+
 }
